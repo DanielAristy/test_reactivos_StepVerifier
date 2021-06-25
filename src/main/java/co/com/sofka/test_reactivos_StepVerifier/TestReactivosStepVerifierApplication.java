4fb1656e7a527e376c6168fab229bc421473efca
DanielAristy <danielaristy22@gmail.com>
2021-06-25 14:11:34 -0500

    Commit inicial con la configuracion necesaria para la configuracion de las pruebas

---
+++ src/main/java/co/com/sofka/test_reactivos_StepVerifier/TestReactivosStepVerifierApplication.java
@@ -0,0 +1,13 @@
+package co.com.sofka.test_reactivos_StepVerifier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestReactivosStepVerifierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestReactivosStepVerifierApplication.class, args);
+	}
+
+}
