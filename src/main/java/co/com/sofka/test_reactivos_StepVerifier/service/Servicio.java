b36c990ced5b63b8711a939e116ea6e133083e33
DanielAristy <danielaristy22@gmail.com>
2021-06-25 14:35:24 -0500

    Probando los tres métodos existentes en la clase de Servicio en los que se utlizan Flux y Mono para declarar los arrays de Strings y donde se ejecutan una prueba para cada uno para la busqueda y verificacion de los valores esperados

---
+++ src/main/java/co/com/sofka/test_reactivos_StepVerifier/service/Servicio.java
@@ -0,0 +1,20 @@
+package co.com.sofka.test_reactivos_StepVerifier.service;
+
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.time.Duration;
+
+@Service
+public class Servicio {
+    public Mono<String> buscarUno() {
+        return Mono.just("Pedro");
+    }
+    public Flux<String> buscarTodos() {
+        return Flux.just("Pedro", "Maria", "Jesus", "Carmen" ,"Daniel");
+    }
+    public Flux<String> buscarTodosLento() {
+        return Flux.just("Pedro", "Maria", "Jesus", "Carmen" ,"Daniel").delaySequence(Duration.ofSeconds(20));
+    }
+}
